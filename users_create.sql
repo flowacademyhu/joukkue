d624ef4a7cce4db12ee51fdc472427d2fb5f1e8f
szabadee <kreativ.szabadi@gmail.com>
2018-11-08 10:12:27 +0100

    users

--- users_create.sql
+++ users_create.sql
@@ -1,1 +1,1 @@
-create table users (id serial primary key, firstname varchar(32), lastname varchar(32), password varchar(16), authorization varchar(32), email varchar(32), date date);
+create table users (id serial primary key, firstname varchar(32), lastname varchar(32), password varchar(16), role varchar(32), email varchar(32), date date);
